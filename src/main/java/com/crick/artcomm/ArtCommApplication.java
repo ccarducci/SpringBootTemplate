fe3b1e6d0c88a44e2d21e01147df42c5ecb53267
ccarducci <cristiano.carducci@gmail.com>
2019-07-19 10:44:11 +0200

    jwt

--- src/main/java/com/crick/artcomm/ArtCommApplication.java
+++ src/main/java/com/crick/artcomm/ArtCommApplication.java
@@ -1,13 +1,24 @@
 package com.crick.artcomm;
 
+import java.security.Principal;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ArtCommApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ArtCommApplication.class, args);
 	}
 
+	@RequestMapping(value = "/user")
+	public Principal user(Principal principal) {
+		return principal;
+	}
+	
 }
